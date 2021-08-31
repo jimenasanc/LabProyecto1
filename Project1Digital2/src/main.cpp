5ec596027acb708facfefc066643d4e7a2c4892e
jimenasanc <san19233@uvg.edu.gt>
2021-08-30 21:32:22 -0600

    implementando boton

--- Project1Digital2/src/main.cpp
+++ Project1Digital2/src/main.cpp
@@ -15,7 +15,8 @@
 //*****************************************************************************************
 //Definición de pines
 //*****************************************************************************************
-#define SensorTemp
+#define SensorTemp 35
+#define b1 25
 
 //*****************************************************************************************
 //Prototipos de funciones
@@ -24,9 +25,11 @@
 //*****************************************************************************************
 //Variables Globales
 //*****************************************************************************************
-int LM35_Sensor = 0;
+int LM35_Sensor = 0; 
 float LM35_Temp_Sensor = 0.0;
 float Voltaje = 0.0;
+
+int b1State = 0;
 
 //*****************************************************************************************
 //ISR
@@ -38,12 +41,15 @@ float Voltaje = 0.0;
 
 void setup() {
   Serial.begin(115200);
+  pinMode(b1, INPUT_PULLUP);
+
 }
 
 //*****************************************************************************************
 //Loop Principal
 //*****************************************************************************************
 void loop() {
+  //para el sensor de temperatura
   LM35_Sensor = analogRead(SensorTemp); //conecta la variable adc float con el pin de salida
   Voltaje = readADC_Cal(LM35_Sensor); // lee adc en voltaje
   LM35_Temp_Sensor = Voltaje / 10; //como el voltaje esta en mV,lo divido entre 10
@@ -51,8 +57,25 @@ void loop() {
   //Imprimir en pantalla de Viasual para comprobar lectura
   Serial.print("Temperatura = ");
   Serial.print("LM35_Temp_Sensor");
-  Serial.print(" °C ")
+  Serial.print(" °C ");
+
   delay(100);
+
+  //implementando boton
+ /* if(digitalWrite(b1)== LOW) //empiezo en low para que se conecte a tierra
+  {
+    b1State = HIGH; 
+  }
+  else if (b1State == HIGH)
+  {
+    b1State = LOW;
+
+  }
+  */
+ if (digitalRead(b1)== 0){
+   digitalWrite(SensorTemp, LOW);
+
+ }
 }
 
 //calibracion y lectura de ADC
