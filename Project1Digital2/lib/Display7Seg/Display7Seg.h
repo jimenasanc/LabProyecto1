8b1eec01e9b13a181e8a89d1f60dedd033fb9f8d
jimenasanc <san19233@uvg.edu.gt>
2021-09-03 13:16:04 -0600

    parte 2 casi completa

---
+++ Project1Digital2/lib/Display7Seg/Display7Seg.h
@@ -0,0 +1,9 @@
+#ifndef __DISPLAY7SEG_H__
+#define __DISPLAY7SEG_H__
+#include <Arduino.h>
+
+void configurarDisplay(uint8_t A, uint8_t B, uint8_t C, uint8_t D, uint8_t E, uint8_t F, uint8_t G, uint8_t dP);
+//Funci[on para desplegar el digito en el display de 7 segmentos
+
+//Funcion]
+#endif // __DISPLAY7SEG_H__
--- Project1Digital2/src/main.cpp
+++ Project1Digital2/src/main.cpp
@@ -19,11 +19,27 @@
 #define SensorTemp 35
 #define b1 25
 
+// parte 2
+#define ledR
+#define ledV
+#define ledA
+
+#define pwmChannel1 1 // canal para led roja
+#define pwmChannel2 2 // canal para led verde
+#define pwmChannel3 3 // canal para led azul
+
+#define freqPWMled 5000 //Frecuencia de led en Hz
+#define resolution 8    //1-16 bits de resolucion
+
+//timer
+#define prescaler 80
+
 
 
 //*****************************************************************************************
 //Prototipos de funciones
 //*****************************************************************************************
+void configurarPWM();
 
 //*****************************************************************************************
 //Variables Globales
@@ -31,6 +47,7 @@
 int LM35_Sensor = 0; 
 float LM35_Temp_Sensor = 0.0;
 float Voltaje = 0.0;
+//float 
 
 int b1State = 0;
 //int modo = 0; //que no empiece la lectura aun
@@ -58,6 +75,10 @@ void setup() {
   Serial.begin(115200);
   pinMode(b1, INPUT_PULLUP);
   attachInterrupt(b1, b1Temp, HIGH);
+  pinMode(ledR, OUTPUT);
+  pinMode(ledV, OUTPUT);
+  pinMode(ledA, OUTPUT);
+  configurarPWM();
 
 }
 
@@ -65,17 +86,38 @@ void setup() {
 //Loop Principal
 //*****************************************************************************************
 void loop() {
- if(digitalWrite(b1)== LOW) //empiezo en low para que se conecte a tierra
+  if(b1State == 1)
   {
-    b1State = HIGH; 
+    b1State = 0;
   }
-  else if (b1State == HIGH)
+  switch()
   {
-    b1State = LOW;
+    case 1:
+    //si la temperatura es menor a 37 °C, el color del semaforo es verde
+    if(LM35_Sensor <= 20.0)
+    {
+      ledcWrite(pwmChannel1, 0);
+      ledcWrite(pwmChannel2, 255);
+      ledcWrite(pwmChannel3, 0);
+    }
+    //si la temperatura esta arriba de 37°C y abajo de 37.5°C , se enciende la led azul
+    if(20 <= LM35_Sensor && LM35_Sensor <= 40)
+    {
+      ledcWrite(pwmChannel1, 0);
+      ledcWrite(pwmChannel2, 0);
+      ledcWrite(pwmChannel3, 255);     
+    }
+    //si la temperatura es mayor a 37.5°C, el color del semaforo es rojo
+    if (LM35_Sensor > 40)
+    {
+      ledcWrite(pwmChannel1, 255);
+      ledcWrite(pwmChannel2, 0);
+      ledcWrite(pwmChannel3, 0);      
+    }
+  }
+  
 
 }
-}
-
 //calibracion y lectura de ADC
 uint32_t readADC_Cal(int ADC_Raw)
 {
@@ -83,4 +125,20 @@ uint32_t readADC_Cal(int ADC_Raw)
   
   esp_adc_cal_characterize(ADC_UNIT_1, ADC_ATTEN_DB_11, ADC_WIDTH_BIT_12, 1100, &adc_chars);
   return(esp_adc_cal_raw_to_voltage(ADC_Raw, &adc_chars));
+}
+
+//*****************************************************************************************
+//Configuración de función de PWM
+//*****************************************************************************************
+void configurarPWM()
+{
+  //Paso 1: Configurar el modulo PWM
+  ledcSetup(pwmChannel1, freqPWMled, resolution);
+  ledcSetup(pwmChannel2, freqPWMled, resolution);
+  ledcSetup(pwmChannel3, freqPWMled, resolution);
+
+    //Paso 2: Relacionar el GPIO y el canal
+  ledcAttachPin(ledR, pwmChannel1);
+  ledcAttachPin(ledV, pwmChannel2);
+  ledcAttachPin(ledA, pwmChannel3);
 }
